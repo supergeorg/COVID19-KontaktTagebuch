d2c1f0250c28fbb46b79c4102e76b5c0e9d07d08
Georg Meißner <Georg.Meissner@mailbox.tu-dresden.de>
2020-08-17 10:55:37 +0200

    Initial Commit

---
+++ Shared/COVID19_KontaktTagebuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVID19_KontaktTagebuchApp.swift
+//  Shared
+//
+//  Created by Georg Meissner on 17.08.20.
+//
+
+import SwiftUI
+
+@main
+struct COVID19_KontaktTagebuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
